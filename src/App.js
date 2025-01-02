b551411e175d001075e912857bb75796d080923e
adarsh <aadarsh.22.2003@gmail.com>
2025-01-02 17:53:53 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./components/auth/Login";
+import JobList from "./components/jobs/JobList";
+import Navbar from "./components/layout/Navbar";
+import Profile from "./components/profile/Profile";
+import Register from "./components/auth/Register";
+import ApplicationList from "./components/applications/ApplicationList";
+import { Toilet } from "lucide-react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AuthProvider>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/jobs" element={<JobList />} />
+            <Route path="/applications" element={<ApplicationList />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/" element={<JobList />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </Router>
   );
-}
+};
 
 export default App;
